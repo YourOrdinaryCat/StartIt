72dce7df31400148e6652ceb3ad5afb95ebf03c7
Omar Plata Salas <omarps464@gmail.com>
2022-03-24 20:14:03 -0500

    Added header, fixed back navigation

--- app/src/main/java/com/yoc/startit/MainScreen.kt
+++ app/src/main/java/com/yoc/startit/MainScreen.kt
@@ -3,14 +3,19 @@ package com.yoc.startit
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
@@ -42,7 +47,22 @@ class MainScreen : ComponentActivity() {
                     color = Color.Black
                 ) {
                     InstalledAppsDataSource.populateAppList(LocalContext.current)
-                    AllAppsGridView(LocalContext.current)
+
+                    Column(
+                        horizontalAlignment = Alignment.Start
+                    ) {
+                        Text(
+                            "StartIt",
+                            color = MaterialTheme.colors.onSurface,
+                            style = MaterialTheme.typography.h1,
+                            modifier = Modifier.padding(12.dp, 24.dp, 0.dp, 0.dp)
+                        )
+                        AllAppsGridView(LocalContext.current)
+                    }
+
+                    BackHandler {
+                        // No need to do anything here
+                    }
                 }
             }
         }
--- app/src/main/java/com/yoc/startit/ui/theme/Type.kt
+++ app/src/main/java/com/yoc/startit/ui/theme/Type.kt
@@ -1,5 +1,6 @@
 package com.yoc.startit.ui.theme
 
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
@@ -8,6 +9,11 @@ import androidx.compose.ui.unit.sp
 
 // Set of Material typography styles to start with
 val Typography = Typography(
+    h1 = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Light,
+        fontSize = 56.sp
+    ),
     body1 = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
