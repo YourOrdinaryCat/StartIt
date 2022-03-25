da2a3ea88638d816462ffc9686fa369dc7905eab
Omar Plata Salas <omarps464@gmail.com>
2022-03-24 19:19:48 -0500

    Padding improvements

--- app/src/main/java/com/yoc/startit/MainScreen.kt
+++ app/src/main/java/com/yoc/startit/MainScreen.kt
@@ -5,10 +5,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.yoc.startit.controls.AppList
@@ -25,7 +25,7 @@ class MainScreen : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    color = Color.Black
                 ) {
                     InstalledAppsDataSource.populateAppList(LocalContext.current)
                     AllAppsGridView(LocalContext.current)
